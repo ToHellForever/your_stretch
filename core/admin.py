ae53dee036a9da7047e61dd1f22408477684facb
ToHellForever <dim.anosoff2018@yandex.ru>
2025-10-27 21:20:03 +0300

    добавил возможность множественной добавки фото в галерею, добавил verbose name в модели, админка готова

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Product, Profile, Gallery, GalleryImage
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+    list_filter = ('price',)
+    search_fields = ('title',)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'area', 'angles', 'profile', 'price')
+    list_filter = ('profile', 'price')
+    search_fields = ('name', 'profile')
+
+class GalleryImageInline(admin.TabularInline):
+    model = GalleryImage
+    extra = 1
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    inlines = [GalleryImageInline]
+    list_display = ('id', 'name')
+    search_fields = ('name',)
--- core/models.py
+++ core/models.py
@@ -3,42 +3,53 @@ from django.db import models
 # модель товара с полями: картинка, название, цена
 class Product(models.Model):
     # картинка
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', verbose_name='Картинка')
     # название
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name='Название')
     # цена
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+
     class Meta:
         verbose_name = 'Товар'
         verbose_name_plural = 'Товары'
         
         
-# примеры работ с полями: картинка, название, площадь, количество углов, профиль, цена 
+# примеры работ с полями: картинка, название, площадь, количество углов, профиль, цена
 class Profile(models.Model):
     # картинка
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', verbose_name='Картинка')
     # название
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='Название')
     # площадь
-    area = models.DecimalField(max_digits=10, decimal_places=2)
+    area = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Площадь')
     # количество углов
-    angles = models.IntegerField()
+    angles = models.IntegerField(verbose_name='Количество углов')
     # профиль
-    profile = models.CharField(max_length=255)
+    profile = models.CharField(max_length=255, verbose_name='Профиль')
     # цена
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
 
     class Meta:
         verbose_name = 'Пример работы'
         verbose_name_plural = 'Примеры работ'
 
 
-# галерея работ с полями: картинка
+# галерея работ
 class Gallery(models.Model):
-    # картинка
-    image = models.ImageField(upload_to='images/')
-    
+    name = models.CharField(max_length=255, verbose_name='Название галереи', blank=True, null=True)
+
     class Meta:
         verbose_name = 'Галерея'
-        verbose_name_plural = 'Галерея'
+        verbose_name_plural = 'Галереи'
+
+    def __str__(self):
+        return self.name or f"Галерея {self.id}"
+
+# фотографии в галерее
+class GalleryImage(models.Model):
+    gallery = models.ForeignKey(Gallery, on_delete=models.CASCADE, related_name='images', verbose_name='Галерея')
+    image = models.ImageField(upload_to='gallery_images/', verbose_name='Картинка')
+
+    class Meta:
+        verbose_name = 'Фотография галереи'
+        verbose_name_plural = 'Фотографии галереи'
