3c2f6a97c62e5c4a3bb632462f61e39b6ebc3e4a
ToHellForever <dim.anosoff2018@yandex.ru>
2025-10-27 21:59:20 +0300

    черновой вариант 'Стоимость услуг'

--- core/views.py
+++ core/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.views.generic import TemplateView
+from .models import Product, Profile, Gallery
 
 
 class LandingView(TemplateView):
     template_name = "landing.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.all()
+        context['profiles'] = Profile.objects.all()
+        context['galleries'] = Gallery.objects.prefetch_related('images').all()
         return context
