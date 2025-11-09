c5e53dfe2af3d729c9fa2e8a55696ad6c90fdbbe
ToHellForever <dim.anosoff2018@yandex.ru>
2025-11-10 01:15:23 +0300

    .

--- your_stretch/settings.py
+++ your_stretch/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
-
+# 
 ROOT_URLCONF = 'your_stretch.urls'
 # 
 TEMPLATES = [
